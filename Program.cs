1145373d1f7d12c464d850ec1cbfd048ff46e4d4
Erick <123erikalluno123@gmail.com>
2026-07-21 14:47:55 -0300

    interfaces

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Clown
+{
+    class TallGuy : IClown
+    {
+        public string Name;
+        public int Height;
+        public string FunnyThingIHave { get {return "big shoes"; } }
+
+        public void TalkAboutYourself()
+        {
+            Console.WriteLine($"Hi, my name is {Name} and I am {Height} inches tall.");
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine("Honk Honk!");
+        }
+    }
+
+    interface IClown
+    {
+        public string FunnyThingIHave { get; }
+
+        void Honk();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TallGuy tallGuy = new TallGuy()
+            {
+                Name = "Big Joe",
+                Height = 72
+            };
+            tallGuy.TalkAboutYourself();
+            Console.WriteLine($"I have {tallGuy.FunnyThingIHave}.");
+            tallGuy.Honk();
+        }
+    }
+}
